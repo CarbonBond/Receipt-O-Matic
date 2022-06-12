f7db4109485e1763831c2c020226287b8cc1cc9e
Carbon <cheeseburgerbbgun@gmail.com>
2022-06-12 13:51:12 -0600

    Fixed seed

---
+++ prisma/seed.js
@@ -0,0 +1,64 @@
+import PrismaClientPkg from '@prisma/client'
+
+// Prisma doesn't ES modules so we have to assign it.
+const PrismaClient = PrismaClientPkg.PrismaClient;
+const prisma = new PrismaClient();
+
+function getUsers() {
+  return [
+    {
+      username: 'carbon',
+      email: 'brandon.w.burge@email.com',
+      firstname: 'Brandon',
+      lastname: 'Burge',
+      role: {
+        create: [
+          {
+            name: 'admin',
+            permission: ['admin']
+          }
+        ]
+      },
+      receipt: {
+        create: [
+          {
+            customer: {
+              create: {
+                firstname: 'Joe',
+                lastname: 'smith'
+              }
+            },
+            products: {
+              create: [
+                {
+                  name: "bike",
+                  cost: 1000.00,
+                  desc: "Mountain bike with shocks and disc brakes",
+                  sku: " 1010101",
+                  quantity: 1
+                }, {
+                  name: "ball",
+                  cost: 1.50,
+                  desc: "Bouncy Ball",
+                  sku: "1111111",
+                  quantity: 100
+                }
+
+              ]
+            }
+          }
+        ]
+      }
+    },
+  ]
+}
+
+async function seed() {
+  await Promise.all(
+    getUsers().map((user) => {
+      return prisma.user.create({ data: user })
+    })
+  )
+}
+
+seed()
